5cecc06c232d6abd883ee2c2921f054b3227fdc1
Samu József <hello@sjozsef.com>
2024-10-07 21:03:30 +0200

    feat: remap hjkl keys to jkl;

--- init.lua
+++ init.lua
@@ -1,10 +1,8 @@
--- Átállítjuk a hjkl billentyűket jkl; billentyűkre
 vim.keymap.set('n', 'j', 'h')
 vim.keymap.set('n', 'k', 'j')
 vim.keymap.set('n', 'l', 'k')
 vim.keymap.set('n', ';', 'l')
 
--- ReloadConfig parancs létrehozása a konfiguráció újratöltéséhez
 vim.api.nvim_create_user_command('ReloadConfig', function()
     for name,_ in pairs(package.loaded) do
         if name:match('^user') then
@@ -15,7 +13,4 @@ vim.api.nvim_create_user_command('ReloadConfig', function()
     vim.notify('Konfiguráció újratöltve', vim.log.levels.INFO)
 end, {})
 
--- Parancs aliasok hozzáadása a gyakori elírások kezelésére
 vim.cmd('command! ReloadCfg ReloadConfig')
-vim.cmd('command! Reloadconfig ReloadConfig')
-vim.cmd('command! ReloadConfiguration ReloadConfig')
