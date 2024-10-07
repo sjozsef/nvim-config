e8adaa421548aae7794ce61c66df2c09d99ee588
Samu József <hello@sjozsef.com>
2024-10-07 23:06:44 +0200

    feat: configure lazy.nvim setup

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,31 +14,3 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   end
 end
 vim.opt.rtp:prepend(lazypath)
-
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
--- Setup lazy.nvim
-require("lazy").setup({
-  spec = {
-    -- import your plugins
-    { import = "plugins" },
-    -- Add telescope.nvim and its dependencies
-    {
-      'nvim-telescope/telescope.nvim',
-      tag = '0.1.5',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
-})
-
--- Load telescope configuration
-require('config.telescope')
