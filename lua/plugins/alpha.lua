270400cb2c689017f0d9eed2a8d840ee478310bb
Samu József <hello@sjozsef.com>
2024-10-08 15:36:33 +0200

    feat: Simplify alpha-nvim configuration

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,34 +1,16 @@
 return {
     'goolord/alpha-nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function ()
-        local alpha = require'alpha'
-        local dashboard = require'alpha.themes.dashboard'
-        dashboard.section.header.val = {
-            [[                               __                ]],
-            [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-            [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-            [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-            [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-            [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
-        }
+    config = function()
+        local alpha = require("alpha")
+        local dashboard = require("alpha.themes.dashboard")
+
         dashboard.section.buttons.val = {
-            dashboard.button("1", "Libra Admin", ":lua vim.fn.chdir(vim.fn.expand('~/Projects/netpeople/admin.librabooks.hu'))<CR>"),
-            dashboard.button("2", "Libra Ui", ":lua vim.fn.chdir(vim.fn.expand('~/Projects/netpeople/ui.librabooks.hu'))<CR>"),
-            dashboard.button("3", "UNMXR App", ":lua vim.fn.chdir(vim.fn.expand('~/Projects/devoid/unmxr/app'))<CR>"),
-            dashboard.button("4", "UNMXR Worker", ":lua vim.fn.chdir(vim.fn.expand('~/Projects/devoid/unmxr/base-services'))<CR>"),
-            dashboard.button("5", "UNMXR Audio Services", ":lua vim.fn.chdir(vim.fn.expand('~/Projects/unmxr/audio-services'))<CR>"),
-            dashboard.button("f", "Find file", ":Telescope find_files<CR>"),
-            dashboard.button("r", "Recent", ":Telescope oldfiles<CR>"),
-            dashboard.button("s", "Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-            dashboard.button("q", "Quit NVIM", ":qa<CR>"),
+            dashboard.button("p l a", "Librabooks Admin", "<cmd>cd ~/Projects/netpeople/admin.librabooks.hu <CR>")
+            dashboard.button("SPC f f", "Find files")
+            dashboard.button("SPC f w", "Live grep")
         }
-        dashboard.section.footer.val = "Happy coding!"
 
-        dashboard.config.opts.noautocmd = true
-
-        vim.cmd[[autocmd User AlphaReady echo 'ready']]
-
-        alpha.setup(dashboard.config)
+        alpha.setup(dashboard.opts)
     end
 }
